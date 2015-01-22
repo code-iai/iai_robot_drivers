9a62d57b2dce83fbeda4cc976427876c11a73c10
Georg Bartels <georg.bartels@cs.uni-bremen.de>
2015-01-22 17:18:56 +0100

    Created a new class for the node.

--- iai_kms_40_driver/src/iai_kms_40_driver/main.cpp
+++ iai_kms_40_driver/src/iai_kms_40_driver/main.cpp
@@ -5,40 +5,57 @@
 
 using namespace iai_kms_40_driver;
 
+class KMS40DriverNode
+{
+  public:
+    KMS40DriverNode(const ros::NodeHandle& nh) : nh_(nh) {}
+
+    ~KMS40DriverNode() {}
+
+    void run()
+    {
+      if(!startUp())
+        return;
+
+      loop();
+
+      driver_.stop();
+    }
+
+  private:
+    ros::NodeHandle nh_;
+    KMS40Driver driver_;
+
+    bool startUp()
+    {
+      // specify a timeout of 1 second
+      struct timeval read_timeout;
+      read_timeout.tv_sec = 1;
+      read_timeout.tv_usec = 0;
+      
+      return driver_.init("192.168.100.175", "1000", read_timeout) && driver_.start();
+    }
+
+    void loop()
+    {
+      ros::Duration d(0.5);
+      d.sleep();
+ 
+      ros::Rate r(1);
+      while(ros::ok())
+      {
+        std::cout << driver_.currentWrench() << std::endl;
+        ros::spinOnce();
+        r.sleep();
+      }
+    }
+};
+
 int main(int argc, char **argv)
 {
   ros::init(argc, argv, "kms40_node");
-  ros::NodeHandle n("~");
-
-  // specify a timeout of 1 second
-  struct timeval read_timeout;
-  read_timeout.tv_sec = 1;
-  read_timeout.tv_usec = 0;
-
-  KMS40Driver my_driver;
-  if(!my_driver.init("192.168.100.175", "1000", read_timeout))
-  {
-    ROS_ERROR("Error initializing kms40 driver.");
-    return 0;
-  }
-
-  if(!my_driver.start())
-  {
-    ROS_ERROR("Error starting kms40 driver.");
-    return 0;
-  }
-
-  ros::Duration d(0.5);
-  d.sleep();
-
-  ros::Rate r(1);
-  while(ros::ok())
-  {
-    std::cout << my_driver.currentWrench() << std::endl;
-    ros::spinOnce();
-    r.sleep();
-  }
-  my_driver.stop();
-
+  ros::NodeHandle nh("~");
+  KMS40DriverNode kms40Node(nh);
+  kms40Node.run();
   return 0;
 }
