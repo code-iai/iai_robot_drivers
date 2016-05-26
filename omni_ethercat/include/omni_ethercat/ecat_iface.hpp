79af3d2ccd3a65d6a35eefb880ffc144d2c941e6
Alexis Maldonado <amaldo@cs.uni-bremen.de>
2016-05-26 09:52:45 +0200

    Add the new code for the refactoring of omni_ethercat

---
+++ omni_ethercat/include/omni_ethercat/ecat_iface.hpp
@@ -0,0 +1,264 @@
+/*
+ * This file is part of the omni_ethercat project.
+ *
+ * Copyright (C) 2012-2016 Alexis Maldonado Herrera <amaldo@cs.uni-bremen.de>
+ * Copyright (C) 2009-2012 Ingo Kresse <kresse@in.tum.de>
+ * Copyright (C) 2009 Uwe Hermann <uwe@hermann-uwe.de>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
+ */
+
+#ifndef OMNI_ETHERCAT_ECAT_IFACE_HPP
+#define OMNI_ETHERCAT_ECAT_IFACE_HPP
+
+#include <pthread.h>
+#include <ecrt.h>  //part of igh's ethercat master
+#include <omni_ethercat/omnilib.hpp>
+
+
+
+
+
+//This order must match *types below
+#define EC_INT8   0
+#define EC_UINT8  1
+#define EC_INT16  2
+#define EC_UINT16 3
+#define EC_INT32  4
+#define EC_UINT32 5
+
+
+
+namespace omni_ecat
+{
+
+
+
+void print_pdo_entry_reg(ec_pdo_entry_reg_t reg);
+//Adjust for different VID/PID
+const uint32_t elmo_vendor_id = 0x0000009a;
+const uint32_t elmo_gold_whistle_product_id = 0x00030924;
+
+class EcatPdoEntry {
+public:
+	uint16_t index;
+	uint8_t subindex;
+	unsigned int *offset;
+	unsigned int *bit_position;
+};
+
+//Memory to hold the offsets to the individual variables of the Process Data
+//(Endianness could differ from CPU depending on architecture, so use the EC_READ, EC_WRITE macros to get data in and out)
+//They are all unsigned int here (and (pd_domain + offset_var) is a uint8_t*),
+// but the data gets casted to the right types using the mentioned EC_READ/EC_WRITE macros
+class DriveProcessDataOffsets {
+public:
+	//vars that we write to the Drives
+	unsigned int w_target_position;
+	unsigned int w_target_velocity;
+	unsigned int w_target_torque;
+	unsigned int w_max_torque;
+	unsigned int w_controlword;
+	unsigned int w_mode_of_operation;
+	unsigned int w_profile_velocity;
+	unsigned int w_profile_acceleration;
+	unsigned int w_profile_deceleration;
+	unsigned int w_digital_outputs;
+	//vars that we read from the Drives
+	unsigned int r_actual_position;
+	unsigned int r_digital_inputs;
+	unsigned int r_actual_velocity;
+	unsigned int r_statusword;
+	unsigned int r_mode_of_operation_display;
+	unsigned int r_actual_torque;
+};
+
+//Registers we read from the drives
+//Has to match the r_* variables in the DriveProcessDataOffsets class
+//Here the variables have their correct type
+class DriveTaskReadVars {
+public:
+	int32_t actual_position;
+	uint32_t digital_inputs;
+	int32_t actual_velocity;
+	uint16_t statusword;
+	int8_t mode_of_operation_display;
+	int16_t actual_torque;
+};
+
+
+//Registers that we will write on the drives
+//Has to match the w_* variables in the DriveProcessDataOffsets class
+//Here the variables have their correct type
+class DriveTaskWriteVars {
+public:
+	//DriveTaskWriteVars();
+	int32_t target_position;
+	int32_t target_velocity;
+	int16_t target_torque;
+	int16_t max_torque;
+	uint16_t controlword;
+	int8_t mode_of_operation;
+	uint32_t profile_velocity;
+	uint32_t profile_acceleration;
+	uint32_t profile_deceleration;
+	uint32_t digital_outputs;
+};
+
+
+
+
+//We will use one instance of EcatELMODrive per motor driver slave on the bus
+class EcatELMODrive {
+public:
+	EcatELMODrive(uint16_t alias, uint16_t position, uint32_t vendor_id, uint32_t product_code);
+	void setup_variables();
+
+	//TODO: Cleanup this interface, an move entries to be private
+
+	//variables to identify the slave
+	uint16_t alias_;
+	uint16_t position_;
+	uint32_t vendor_id_;
+	uint32_t product_code_;
+
+	ec_slave_config_t *sc;
+	ec_slave_config_state_t slave_config_state;
+	ec_slave_config_state_t slave_config_state_old; //rename to old_state
+
+
+	DriveProcessDataOffsets pdata_offsets;   //memory the ECAT master writes to directly
+
+
+	DriveTaskReadVars task_rdata_process_side;
+	DriveTaskReadVars task_rdata_user_side;
+	DriveTaskWriteVars task_wdata_process_side;
+	DriveTaskWriteVars task_wdata_user_side;
+
+
+	//add_pdo_entry stores a desired pdo mapping into pdos_
+	void add_pdo_entry(uint16_t index, uint8_t subindex, unsigned int *offset, unsigned int *bit_position);
+
+	std::vector<ec_pdo_entry_reg_t> get_pdo_entry_regs();
+	void print_pdo_entry_regs();
+
+	//Methods that copy in/out of the buffers
+	void task_data_to_user_data();
+	void user_data_to_task_data();
+
+	//Methods that use EC_READ and EC_WRITE to get data out/in of the process variables
+	void process_to_task_data();
+	void task_data_to_process();
+
+	void set_domain_pd(uint8_t *domain_pd);
+
+
+private:
+
+	//pdos_ holds a list of the partial pdo entry (to be completed with the variables that identify the slave)
+	std::vector<EcatPdoEntry> pdos_;
+
+	uint8_t *domain_pd_;  //holds the address to which the offsets are relative
+
+};
+
+//Helper class to control the EtherCAT master, and talk to the slaves
+class EcatAdmin {
+public:
+	EcatAdmin();
+	int ecat_init();  //Should be the first thing to be called after instantiating. Check the return value to decide what to do afterwards.
+	bool finished_ecat_init();  // Will return true if the ecat_init has finished happily
+
+	void check_drive_state();   //should be called periodically to check on the state of the drives (and reactivates them if they went down)
+	void shutdown();
+
+	void print_pdo_entries();
+
+
+
+
+private:
+	//Creates the pdo_entry table
+	void prepare_objects_for_slaves_on_boxy();
+
+	int start_omni_realtime();
+	int stop_omni_realtime();
+
+	void realtime_main();  //Function that will run in a thread with realtime priority
+	void cyclic_ecat_task();  //gets called by realtime_main once per cycle, and does the ethercat regular chores
+	int cyclic_counter;
+	uint64_t ecat_cycle_counter;
+
+	//FIXME: Need to add functionity for drives that need to home (like the torso one)
+	//void configure_torso_drive();
+
+	void ec_drives_poweroff();
+	void ec_drives_speedcontrol();
+	void ec_drives_recover();
+	void ec_drives_poweron();
+
+	std::vector<EcatELMODrive*> drives_;  //Holds the addresses of all the etherCAT slaves
+	unsigned int num_drives_;
+
+	EcatELMODrive* drive_fr_;
+	EcatELMODrive* drive_fl_;
+	EcatELMODrive* drive_br_;
+	EcatELMODrive* drive_bl_;
+	EcatELMODrive* drive_torso_;
+
+	bool finished_ecat_init_;
+
+	//helper function to write to registers using SDO
+	int ecat_writeSDO(int device, int index, int subindex, int value, int type);
+
+	void check_domain1_state();
+	void check_master_state(void);
+	void check_slave_config_states(void);
+
+
+	std::vector<ec_pdo_entry_reg_t> get_pdo_entry_regs();
+	std::vector<ec_pdo_entry_reg_t> get_pdo_entry_regs_terminated();
+
+
+	//variables used in the realtimeMain
+	int realtime_cycle_period_ns; 	//This variable in nanoseconds defines the ethercat cycle time
+	int rt_should_exit;
+	int rt_misses;
+	pthread_mutex_t rt_data_mutex;
+
+	ec_master_t *ec_master = NULL;
+	ec_master_state_t master_state;// = { };
+
+	ec_domain_t *domain1 = NULL;
+	ec_domain_state_t domain1_state;// = { };
+
+	/* Process data */
+	uint8_t *domain1_pd;	/* Process data memory */
+
+	pthread_t rt_thread;
+	//Entry function needed to create the realtime thread using pthread_create and point to a member
+	static void * realtimeMainEntryFunc(void * This) {((EcatAdmin *)This)->realtime_main(); return NULL;}
+
+	bool ec_domain_running;
+
+
+};
+
+
+}
+
+
+
+#endif // OMNI_ETHERCAT_ECAT_IFACE_HPP
---
+++ omni_ethercat/src/omni_ethercat2.cpp
@@ -0,0 +1,410 @@
+/*
+ * This file is part of the omni_ethercat project.
+ *
+ * Copyright (C) 2012-2016 Alexis Maldonado Herrera <amaldo@cs.uni-bremen.de>
+ * Copyright (C) 2009-2012 Ingo Kresse <kresse@in.tum.de>
+ * Copyright (C) 2009 Uwe Hermann <uwe@hermann-uwe.de>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <ros/ros.h>
+#include <geometry_msgs/TwistStamped.h>
+#include <geometry_msgs/Twist.h>
+#include <geometry_msgs/Vector3.h>
+#include <soft_runstop/Handler.h>
+#include <tf/transform_broadcaster.h>
+#include <diagnostic_updater/diagnostic_updater.h>
+//#include <iai_control_msgs/PowerState.h>
+#include <omni_ethercat/omnilib.hpp>   //library for the mecanum kinematics
+#include <omni_ethercat/ecat_iface.hpp>  //library for interfacing with the Ethercat Motor drivers
+
+
+//For the torso:
+//#include <sensor_msgs/JointState.h>
+//#include <std_msgs/Float64.h>
+
+class Omnidrive
+{
+private:
+	ros::NodeHandle n_;
+	diagnostic_updater::Updater diagnostic_;
+	//ros::Publisher current_pub_;
+	//ros::Publisher power_pub_;
+	//ros::Publisher js_pub_; //torso
+	//ros::Subscriber power_sub_;
+	ros::Time watchdog_time_;
+	double drive_[3], drive_last_[3];
+	//double torso_des_pos_; // torso
+	//bool fresh_torso_des_pos_; //torso
+	soft_runstop::Handler soft_runstop_handler_;
+	std::string odom_frame_id_;
+	std::string odom_child_frame_id_;
+	//std::string power_name_;
+	void cmdArrived(const geometry_msgs::TwistStamped::ConstPtr& msg);
+	//void torsoCmdArrived(const std_msgs::Float64::ConstPtr& msg); //torso
+	//void stateUpdate(diagnostic_updater::DiagnosticStatusWrapper &s);
+	//void powerCommand(const iai_control_msgs::PowerState::ConstPtr& msg);
+
+	//Variables to hold the desired twist
+	omni_ethercat::Twist2d des_twist_;
+	omni_ethercat::Twist2d limited_twist_;
+	omni_ethercat::Twist2d max_twist_;
+	double max_wheel_speed_;
+	omni_ethercat::JacParams jac_params_;
+
+public:
+	Omnidrive();
+	void main();
+};
+
+Omnidrive::Omnidrive() : n_("omnidrive"), diagnostic_(), soft_runstop_handler_(Duration(0.5))
+{
+	//diagnostic_.setHardwareID("omnidrive");
+	//diagnostic_.add("Base", this, &Omnidrive::stateUpdate);
+	n_.param("odom_frame_id", odom_frame_id_, std::string("/odom"));
+	n_.param("odom_child_frame_id", odom_child_frame_id_, std::string("/base_footprint"));
+	//n_.param("power_name", power_name_, std::string("Wheels"));
+
+
+	//power_pub_ = n_.advertise<iai_control_msgs::PowerState>("/power_state", 1);
+	//power_sub_ = n_.subscribe<iai_control_msgs::PowerState>("/power_command", 16, &Omnidrive::powerCommand, this);
+
+	//js_pub_ = n_.advertise<sensor_msgs::JointState>("/torso/joint_states", 1);  //torso
+
+	//initialize the twists to all zeroes
+	des_twist_ = omni_ethercat::Twist2d(0,0,0);
+	limited_twist_ = omni_ethercat::Twist2d(0,0,0);
+	max_twist_ = omni_ethercat::Twist2d(1.0, 1.0, 1.0); //FIXME: read from param
+	double max_wheel_tick_speed = 833333.0; // ticks/s : 5000 rpm/ 60s * 10000 ticks/rev
+	double lx = 0.39225;
+	double ly = 0.303495;
+	double drive_constant = 626594.7934; // in ticks/m
+	max_wheel_speed_ = max_wheel_tick_speed; //FIXME: read from param, specify units (rads/s?)
+	jac_params_ = omni_ethercat::JacParams(lx, ly, drive_constant);  // lx, ly, drive-constant in ticks/m
+
+
+	//std::cout << limited_twist_ << std::endl;
+
+
+
+
+	//for(int i=0; i < 3; i++) {
+	//  drive_last_[i] = 0;
+	//  drive_[i] = 0;
+	//}
+
+	watchdog_time_ = ros::Time::now();
+}
+
+void Omnidrive::cmdArrived(const geometry_msgs::TwistStamped::ConstPtr& msg)
+{
+	// FIXME: use TwistStamped instead of Twist and check that people command in the right frame
+	// NOTE: No need for synchronization since this is called inside spinOnce() in the main loop
+
+	//Checking that the frame is correct
+	if (msg->header.frame_id == odom_child_frame_id_) {
+		//The frame in the message matches the one of the base
+		//read desired twist for the base
+		des_twist_ = omni_ethercat::fromTwistMsg(msg->twist);
+		//limit to our maximum values
+		limited_twist_ = omni_ethercat::limitTwist(des_twist_, max_twist_);
+		//limit again if any wheel exceeds the maximum wheel speed
+		limited_twist_ = omni_ethercat::limitTwist(limited_twist_, jac_params_, max_wheel_speed_);
+
+		if (des_twist_ != limited_twist_) {
+			std::cout << "The desired twist will be limited from: " << des_twist_ << " to: " << limited_twist_ << std::endl;
+		}
+
+		watchdog_time_ = ros::Time::now();
+	} else {
+		std::cerr << "Twist command arrived expressed in a wrong frame.";
+	}
+
+
+}
+
+//torso:
+//FIXME: need param to turn torso into velocity or position resolved
+//FIXME: All topics receiving commands need a watchdog, this one too
+
+//void Omnidrive::torsoCmdArrived(const std_msgs::Float64::ConstPtr& msg)
+//{
+//
+//  printf("Desired torso position: %f\n", msg->data);
+//  torso_des_pos_ = msg->data * torso_ticks_to_m;
+//  fresh_torso_des_pos_ = true;
+//
+//
+//}
+
+
+//void Omnidrive::stateUpdate(diagnostic_updater::DiagnosticStatusWrapper &s)
+//{
+//  int estop;
+//  char drive[5]; //+1 for torso
+//  omnidrive_status(&drive[0], &drive[1], &drive[2], &drive[3], &drive[4], &estop);
+//
+//  bool operational = (drive[0] == '4' &&
+//                      drive[1] == '4' &&
+//                      drive[2] == '4' &&
+//                      drive[3] == '4' &&
+//                      drive[4] == '4' &&
+//                      estop == 0);
+//
+//  if(operational)
+//    s.summary(0, "Operational");
+//  else
+//    s.summary(1, "Down");
+//  
+//  for(int i=0; i < num_drives; i++)
+//    s.add(std::string("status drive ") + (char) ('1' + i),
+//          std::string("") + drive[i]);
+//  s.add("Emergency Stop", (estop) ? "== pressed ==" : "released");
+//
+//  commstatus_t comm = omnidrive_commstatus();
+//
+//  for(int i=0; i < num_drives; i++)
+//    s.addf(std::string("comm status drive ")+(char) ('1' + i),
+////    s.addf("cstatus drive",
+//           "0x%02X, %s, %s operational",
+//             comm.slave_state[i],
+//             comm.slave_online[i] ? "online" : "offline",
+//             comm.slave_operational[i] ? "" : "not");
+//
+//  s.addf("master state", "Link is %s, %d slaves, AL states: 0x%02X",
+//           comm.master_link ? "up" : "down",
+//           comm.master_slaves_responding,
+//           comm.master_al_states);
+//
+//  s.addf("working counter", "%d, %s",
+//           comm.working_counter,
+//           comm.working_counter_state == 2 ? "complete" : "incomplete");
+//
+//  iai_control_msgs::PowerState power;
+//  power.name = power_name_;
+//  power.enabled = operational;
+//
+//  power_pub_.publish(power);
+//}
+
+
+////FIXME: Do we need this for this base? This allows to bring the ethercat drives down and up at wish
+//void Omnidrive::powerCommand(const iai_control_msgs::PowerState::ConstPtr& msg)
+//{
+//  if(msg->name == power_name_)
+//  {
+//    printf("Received power command!\n");
+//    int estop;
+//    char drive[4];
+//    omnidrive_status(&drive[0], &drive[1], &drive[2], &drive[3], &drive[4], &estop);
+//
+//    bool power_state = (drive[0] == '4' &&
+//                        drive[1] == '4' &&
+//                        drive[2] == '4' &&
+//                        drive[3] == '4' &&
+//                        drive[4] == '4' &&
+//                        estop == 0);
+//                        
+//    printf("Power_state=%d    drive[0]=%d\n", power_state, drive[0]);
+//
+//    if(msg->enabled == true && power_state == false)
+//    {
+//      printf("Recovering\n");
+//      omnidrive_recover();
+//      omnidrive_poweron();
+//    }
+//
+//    if(msg->enabled == false && power_state == true)
+//    {
+//      printf("Turning off\n");
+//      omnidrive_poweroff();
+//    }
+//    
+//  }
+//}
+
+void Omnidrive::main()
+{
+	double speed, acc_max, t, radius, drift;
+	int tf_frequency, runstop_frequency, js_frequency;
+	const int loop_frequency = 250; // 250Hz update frequency
+
+	//n_.param("speed", speed, 100.0); // 0.1
+	// default acc: brake from max. speed to 0 within 1.5cm
+	//n_.param("acceleration", acc_max, 1000.0);  //0.5*speed*speed/0.015
+
+	n_.param("tf_frequency", tf_frequency, 50);
+	n_.param("js_frequency", js_frequency, 125);
+	n_.param("runstop_frequency", runstop_frequency, 10);
+	n_.param("watchdog_period", t, 0.15);
+	ros::Duration watchdog_period(t);
+	n_.param("odometry_correction", drift, 1.0);
+
+
+
+	omni_ecat::EcatAdmin ecat_admin;
+	int init_worked = ecat_admin.ecat_init();
+
+	if (init_worked != 0) {
+		ROS_ERROR("failed to initialize the ethercat bus");
+		ROS_ERROR("check dmesg and try \"sudo /etc/init.d/ethercat restart\"");
+		return;
+	}
+
+	//tf::TransformBroadcaster transforms;
+
+	ros::Subscriber sub = n_.subscribe("/base/cmd_vel", 10, &Omnidrive::cmdArrived, this);
+	//ros::Subscriber sub_torso = n_.subscribe("/torso/cmd_vel", 10, &Omnidrive::torsoCmdArrived, this); //torso
+	//ros::Publisher hard_runstop_pub = n_.advertise<std_msgs::Bool>("/hard_runstop", 1);
+
+	//double x=0, y=0, a=0, torso_pos=0;
+
+	int tf_publish_counter=0;
+	int tf_send_rate = loop_frequency / tf_frequency;
+
+
+	//torso:
+	int js_publish_counter=0;
+	int js_send_rate = loop_frequency / js_frequency;
+
+	int runstop_publish_counter=0;
+	int runstop_send_rate = loop_frequency / runstop_frequency;
+
+	ros::Rate r(loop_frequency);
+	ros::Time last_drive_check_time = ros::Time::now();
+
+	while(n_.ok()) {
+
+		// Check if we need to enable the drives once a second
+		if ( ecat_admin.finished_ecat_init() && (ros::Time::now() - last_drive_check_time) > ros::Duration(1.0)) {
+			last_drive_check_time = ros::Time::now();
+			ecat_admin.check_drive_state();
+		}
+
+		//omnidrive_odometry(&x, &y, &a, &torso_pos);
+
+
+		//FIXME: Do we need a watchdog for the torso?
+
+		//The watchdog for the /cmd_vel topic
+		//should stop the base if now new messages arrive
+		if( (( ros::Time::now() - watchdog_time_) > watchdog_period) || soft_runstop_handler_.getState()) {
+
+			//printf("Watchdog!\n");
+			//printf("State of the soft_runstop = %d\n", soft_runstop_handler_.getState());
+
+
+			//Only send the ROS warning the first time
+			//when it had some driving velocities, and getting here not because of
+			//the runstop
+			if((drive_[0] != 0 || drive_[1] != 0 || drive_[2] !=0)
+					&& !soft_runstop_handler_.getState())
+				ROS_WARN("engaged watchdog!");
+
+			//zero the command twist
+			limited_twist_ = omni_ethercat::Twist2d(0.0, 0.0, 0.0);
+
+			//While the watchdog is active, revisit here after watchdog_period
+			watchdog_time_ = ros::Time::now();
+		}
+
+
+		//Evil acceleration limitation
+		// this runs in a slow loop
+		//FIXME: Move to the high-speed loop for smoothness -> Maybe not needed, this loop is 250Hz.
+		//FIXME: Limit in twist-space, make it use time
+
+		//    for(int i=0; i < 3; i++) {
+		//      // acceleration limiting
+		//      double acc = drive_[i] - drive_last_[i];
+		//      double fac_rot = (i == 2) ? 1.0/radius : 1.0;
+		//      acc = LIMIT(acc, acc_max*fac_rot*fac_rot);
+		//      drive_[i] = drive_last_[i] + acc;
+		//
+		//      // velocity limiting
+		//      drive_[i] = LIMIT(drive_[i], speed*fac_rot);
+		//
+		//      drive_last_[i] = drive_[i];
+		//    }
+
+		//if the watchdog was activated drive_[0-2] are 0.0
+		//only call omnidrive_drive once in the loop
+		//the last call will probably override the previous ones
+		//    omnidrive_drive(drive_[0], drive_[1], drive_[2], torso_des_pos_);
+
+		// publish odometry readings
+		//    if(++tf_publish_counter == tf_send_rate) {
+		//      tf::Quaternion q;
+		//      q.setRPY(0, 0, a);
+		//      tf::Transform pose(q, tf::Point(x, y, 0.0));
+		//      // FIXME: publish this on a separate topic like /base/odom
+		//      transforms.sendTransform(tf::StampedTransform(pose, ros::Time::now(), frame_id_, child_frame_id_));
+		//      // FIXME: publish actual base twist on topic like /base/vel
+		//      tf_publish_counter = 0;
+		//    }
+		//
+		//
+		//    // publish torso position
+		//    if(++js_publish_counter == js_send_rate) {
+		//      sensor_msgs::JointState msg;
+		//      msg.header.stamp = ros::Time::now();
+		//      msg.name.push_back("triangle_base_joint");
+		//      msg.position.push_back(torso_pos);
+		//      // FIXME: report the actual values
+		//      msg.velocity.push_back(0.0);
+		//      msg.effort.push_back(0.0);
+		//      js_pub_.publish(msg);
+		//      js_publish_counter = 0;
+		//    }
+		//
+		//    // publish hard runstop state
+		//    // FIXME: report real hard E-stop status
+		//    // in Rosie, the hard runstop was read from the ethercat drives
+		//    // in Boxy it is part of the state reported by the ELMO drives
+		//    if(++runstop_publish_counter == runstop_send_rate) {
+		//      int runstop=0;
+		//      omnidrive_status(0,0,0,0,0, &runstop);
+		//      std_msgs::Bool msg;
+		//      msg.data = (runstop != 0);
+		//      hard_runstop_pub.publish(msg);
+		//      runstop_publish_counter = 0;
+		//    }
+
+		// process incoming messages
+		ros::spinOnce();
+
+		//diagnostic_.update();
+		r.sleep();
+
+	}
+
+	ecat_admin.shutdown();
+
+}
+
+
+int main(int argc, char *argv[])
+{
+	ros::init(argc, argv, "omni_ethercat");
+	std::cout << "Starting up\n"  ;
+
+
+	Omnidrive drive;
+	drive.main();
+
+
+}
+
+
