2fc0c4e6ad66541e770ea992e8023bf456bdbb09
Alexis Maldonado <amaldo@cs.uni-bremen.de>
2016-05-26 09:55:08 +0200

    Corrected authors in omni_ethercat2.cpp

--- omni_ethercat/src/omni_ethercat2.cpp
+++ omni_ethercat/src/omni_ethercat2.cpp
@@ -3,7 +3,6 @@
  *
  * Copyright (C) 2012-2016 Alexis Maldonado Herrera <amaldo@cs.uni-bremen.de>
  * Copyright (C) 2009-2012 Ingo Kresse <kresse@in.tum.de>
- * Copyright (C) 2009 Uwe Hermann <uwe@hermann-uwe.de>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
