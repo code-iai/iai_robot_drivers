60f721f0cc198fba28da138e168f73c604e20393
Alexis Maldonado <amaldo@cs.uni-bremen.de>
2017-03-24 11:29:50 +0000

    Fixed compilation in Ubuntu16.04 -> std::isnan

--- wsg_50_driver/src/main.cpp
+++ wsg_50_driver/src/main.cpp
@@ -145,17 +145,17 @@ void timer_cb(const ros::TimerEvent& ev)
   
 	// ==== Call custom measure-and-move command ====
 	int res = 0;
-	if (!isnan(g_goal_position)) {
+	if (!std::isnan(g_goal_position)) {
 		ROS_INFO("Position command: pos=%5.1f, speed=%5.1f", g_goal_position, g_speed);
         res = script_measure_move(1, g_goal_position, g_speed, info);
-	} else if (!isnan(g_goal_speed)) {
+	} else if (!std::isnan(g_goal_speed)) {
 		ROS_INFO("Velocity command: speed=%5.1f", g_goal_speed);
         res = script_measure_move(2, 0, g_goal_speed, info);
 	} else
         res = script_measure_move(0, 0, 0, info);
-	if (!isnan(g_goal_position))
+	if (!std::isnan(g_goal_position))
 		g_goal_position = NAN;
-	if (!isnan(g_goal_speed))
+	if (!std::isnan(g_goal_speed))
 		g_goal_speed = NAN;
 
 	if (!res) {
