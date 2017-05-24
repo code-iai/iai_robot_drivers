dc14369545f5995d246b9f7de405a779239c7ad5
ichumuh <stelter@tzi.de>
2017-05-24 14:44:27 +0200

    changed default for right finger name

--- wsg_50_driver/src/main.cpp
+++ wsg_50_driver/src/main.cpp
@@ -231,7 +231,7 @@ int main( int argc, char **argv )
    nh.param("protocol", protocol, std::string(""));
    nh.param("rate", rate, 1.0); // With custom script, up to 30Hz are possible
    nh.param("grasping_force", grasping_force, 0.0);
-   nh.param("r_finger_joint_name", r_finger_joint_name, std::string("wsg_50_gripper_base_joint"));
+   nh.param("r_finger_joint_name", r_finger_joint_name, std::string("wsg_50_gripper_base_joint_gripper_right"));
    nh.param("reference_frame", reference_frame, std::string("wsg_50_gripper_base_link"));
 	
    if (protocol == "udp")
