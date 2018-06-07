6c8227fd3b82925d15373a40b24bf019bc6ec384
Alexis Maldonado <amaldo@cs.uni-bremen.de>
2018-06-07 09:55:29 +0200

    Fine tune wheel-size on donbot

--- omni_ethercat/src/omni_ethercat2.cpp
+++ omni_ethercat/src/omni_ethercat2.cpp
@@ -80,8 +80,10 @@ Omnidrive::Omnidrive() : n_("omnidrive"), diagnostic_(), soft_runstop_handler_(D
         // calculated thus: 10000 (ticks/turn given by encoder) * gear ratio / ( pi * diameter)
         // diameter is 8" = 8*25.4/1000 = 0.797965m
         // circumference for that 8" wheel is = 0.638372m
+        // For donbot: drive_constant = 10000*20/(3.14159*8*25.4/1000)
 	//double drive_constant = 626594; // in ticks/m  For Boxy (gear ratio 40:1)
-        double drive_constant = 313297; // in ticks/m  For donbot (gear ratio 20:1)
+        //for donbot: adjusting the 0.5% error in translation when moving
+        double drive_constant = 10000*20/(3.14159*8*1.005*25.4/1000); // in ticks/m 
 
 
 	max_wheel_speed_ = max_wheel_tick_speed; //FIXME: read from param, specify units (rads/s?)
@@ -320,14 +322,6 @@ void Omnidrive::main()
 			old_twist = limited_twist_;
 			ROS_INFO_STREAM("Commanded twist: " << limited_twist_.format(CommaInitFmt));
 		}
-
-
-		//Evil acceleration limitation
-		// this runs in a slow loop
-        //FIXME: Add an acceleration limitation in the high-speed loop.
-        //FIXME: Consider switching elmo controller loop to synchronic velocity commands instead of profiled velocity
-		//FIXME: Move to the high-speed loop for smoothness -> Maybe not needed, this loop is 250Hz.
-		//FIXME: Limit in twist-space, make it use time
 
 
         // publish odometry readings
